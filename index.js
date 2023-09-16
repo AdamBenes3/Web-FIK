111c831f5b6431970125eee34b39f147187c881c
Adam Beneš <ad.benes@gmail.com>
2023-09-16 21:17:23 +0200

    Data reader fixed and displayed on web. Save to data.json and also to dataHistory.json

--- index.js
+++ index.js
@@ -2,46 +2,68 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const fs = require('fs');
+const mqtt = require('mqtt');
+const config = require('./config.json'); // Assuming config.json is in the same directory
 
-// Read the configuration from the JSON file
-const configPath = './config.json';
+const serverAddress = 'eu1.cloud.thethings.network:1883';
+const username = config.username;
+const password = config.password;
 
-let ttnEndpoint, apiKey;
+// Replace with your TTN Application ID, Device ID, and the specific topic you want to subscribe to
+const applicationId = 'balloons';
+const deviceId = 'fik8b';
+const topic = `#`;
 
-try {
-  const configData = fs.readFileSync(configPath, 'utf-8');
-  const config = JSON.parse(configData);
-  ttnEndpoint = config.ttnEndpoint;
-  apiKey = config.apiKey;
-} catch (err) {
-  console.error('Error reading configuration file:', err);
-  process.exit(1); // Exit the application if there's an error reading the config file
-}
+// Create an MQTT client instance
+const client = mqtt.connect(`mqtt://${serverAddress}`, {
+    username,
+    password,
+});
 
+// Handle MQTT connection events
+client.on('connect', () => {
+    console.log('Connected to TTN MQTT broker');
 
-// Fetch data from The Things Network
-const fetchData = async () => {
-    try {
-        const response = await axios.get(ttnEndpoint, {
-            headers: {
-                Authorization: `Bearer ${apiKey}`,
-            },
-        });
+    // Subscribe to the specified topic
+    client.subscribe(topic, (error) => {
+        if (!error) {
+            console.log(`Subscribed to topic: ${topic}`);
+        } else {
+            console.error(`Subscription error: ${error}`);
+        }
+    });
+});
 
-        const data = response.data;
-        return data;
-    } catch (error) {
-        console.error('Error fetching data:', error);
-        throw error;
-    }
-};
+client.on('message', (topic, message) => {
+    console.log(`Received message on topic: ${topic}, Message: ${message.toString()}`);
+    // Handle the received message
+    const receivedData = {
+        topic,
+        message: message.toString(),
+        timestamp: new Date().toISOString(),
+    };
 
-// Mock data
-const mockData = {
-    temperature: 25.5,
-    height: 60,
-    sensorName: "Example Sensor",
-};
+    // Convert the data to JSON format
+    const jsonData = JSON.stringify(receivedData, null, 2);
+
+    // Write the data to the data.json file, overwriting its contents
+    fs.writeFileSync('data.json', jsonData);
+
+    // Append the data to a dataHistory.json file
+    fs.appendFile('dataHistory.json', jsonData + '\n', (err) => {
+        if (err) {
+            console.error('Error appending data to dataHistory.json:', err);
+        } else {
+            console.log('Data appended to dataHistory.json');
+        }
+    });
+});
+
+// Handle MQTT connection errors
+client.on('error', (error) => {
+    console.error(`MQTT connection error: ${error}`);
+});
+
 
 
 app.set('view engine', 'ejs'); // Set the view engine to EJS
@@ -49,9 +71,14 @@ app.set('views', 'views'); // Set the directory for EJS templates
 
 app.get('/', async (req, res) => {
     try {
-        //const rawData = await fetchData();
-        //const decodedData = decodePayload(rawData);
-        res.render('index', { data: mockData });
+
+        // Read data from the data.json file
+        const rawData = fs.readFileSync('data.json', 'utf8');
+        const decodedData = JSON.parse(rawData);
+
+        //const decodedData = decodePayload(mockData);
+        res.render('index', { data: decodedData });
+        //res.render('index', { data: mockData });
     } catch (error) {
         res.status(500).send('Error rendering the page');
     }
