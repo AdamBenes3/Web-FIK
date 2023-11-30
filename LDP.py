35c3e03e5d9853f9f999e24019b21b394cfba220
Vit Hanousek <vithanousek@seznam.cz>
2023-11-30 14:31:42 +0100

    switch to production url

--- LDP.py
+++ LDP.py
@@ -16,7 +16,9 @@ password = config['password']
 
 topic = '#'
 balloons_dict = {}
-express_base_url = "http://localhost:4000"
+#express_base_url = "http://localhost:4000"
+express_base_url = "https://fik.crreat.eu"
+
 
 def heartbeat_loop():
     while True:
