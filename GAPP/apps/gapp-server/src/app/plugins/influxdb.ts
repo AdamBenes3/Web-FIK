779b9aabe272871276141d603ee9275c8249dbec
Vilém Raška <v.rashka.ext@make.com>
2025-01-22 14:50:46 +0100

    feat: add influx plugin

---
+++ GAPP/apps/gapp-server/src/app/plugins/influxdb.ts
@@ -0,0 +1,12 @@
+import { FastifyInstance } from 'fastify';
+import fp from 'fastify-plugin';
+import { InfluxDB } from '@influxdata/influxdb-client';
+
+export default fp(async (fastify: FastifyInstance, config) => {
+  const influxClient = new InfluxDB({
+    token: '',
+    url: ''
+  });
+
+  fastify.decorate('influxClient', influxClient);
+});
