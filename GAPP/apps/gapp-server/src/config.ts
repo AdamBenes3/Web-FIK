87950a41802c157fec12832fd9a758b9f8dd19e6
Vilém Raška <v.rashka.ext@make.com>
2025-01-22 13:20:51 +0100

    wip: add influx

--- GAPP/apps/gapp-server/src/config.ts
+++ GAPP/apps/gapp-server/src/config.ts
@@ -1,8 +1,11 @@
-import { cleanEnv, num, str } from 'envalid';
+import { cleanEnv, num, str, url } from 'envalid';
 
 const envSchema = {
   NODE_ENV: str({ choices: ['development', 'production']}),
   PORT: num({ default: 3000 }),
+
+  INFLUXDB_TOKEN: str({ desc: 'Influx db token' }),
+  INFLUXDB_HOST: url({ desc: 'Influx db host with port', devDefault: 'http://localhost:8086' }),
 };
 
 export const getConfig = (configObject: object) => {
--- GAPP/apps/gapp-server/src/main.ts
+++ GAPP/apps/gapp-server/src/main.ts
@@ -1,6 +1,7 @@
 import Fastify from 'fastify';
 import { app } from './app/app';
 import { getConfig } from './config';
+import { InfluxDB, Point } from '@influxdata/influxdb-client';
 
 const config = getConfig(process.env);
 
@@ -16,3 +17,15 @@ server.listen({ port: config.PORT, host: '0.0.0.0' }, (err) => {
         process.exit(1);
     }
 });
+
+
+const client = new InfluxDB({ url: config.INFLUXDB_HOST, token: config.INFLUXDB_TOKEN });
+
+const writeApi = client.getWriteApi('FikFlights', 'Fik', 'ns');
+
+const point = new Point('measurement');
+
+point.tag('location', 'PRG')
+point.intField('humidity', 185);
+
+writeApi.writePoint(point);
