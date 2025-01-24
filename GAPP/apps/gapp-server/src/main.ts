d3e3d5796541e70e39c68047e5cdcb2f0e63e737
Vilém Raška <v.rashka.ext@make.com>
2025-01-24 21:05:23 +0100

    feat: initial working implementation of sondehub library finished

--- GAPP/apps/gapp-server/src/main.ts
+++ GAPP/apps/gapp-server/src/main.ts
@@ -1,25 +1,6 @@
 import Fastify from 'fastify';
 import { app } from './app/app';
 import { getConfig } from './config';
-import { Uploader } from '@gapp/sondehub';
-
-const uploader = new Uploader({ uploader_callsign: 'node-sondehub' });
-
-uploader.addTelemetry({
-    payload_callsign: 'cesilko-payload',
-    datetime: new Date().toISOString(),
-    lat: 50,
-    lon: 15,
-    alt: 2000,
-});
-
-uploader.uploadTelemetry();
-
-uploader.uploadStationPosition({
-  uploader_callsign: 'cesilko-car',
-  uploader_position: [50.01, 15.01, 185],
-  mobile: true
-});
 
 const config = getConfig(process.env);
 
@@ -34,4 +15,9 @@ server.listen({ port: config.PORT, host: '0.0.0.0' }, (err) => {
         server.log.error(err);
         process.exit(1);
     }
+});
+
+process.on('SIGINT', async () => {
+    await server.close();
+    server.log.info('Server stopped');
 });
